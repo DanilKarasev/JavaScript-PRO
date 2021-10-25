f7a36e2ad4cec3485e1428eaa967b1fc742a36ee
DanilKarasev <88264176+DanilKarasev@users.noreply.github.com>
2021-10-25 18:19:44 +1000

    Добавил тестовые классы корзины и немного событий на страницу.

--- Lesson 2/script.js
+++ Lesson 2/script.js
@@ -4,18 +4,15 @@ class GoodsItem {
         this.title = title;
         this.price = price;
     }
-
     render() {
         return `<div class="goods-item"><img src="images/${this.img}.png" alt="${this.img}"><h3>${this.title}</h3><p>$${this.price}</p></div>`;
     }
 }
 
 class GoodsList {
-    x;
     constructor() {
         this.goods = [];
     }
-
     FetchGoods () {
         this.goods = [
             { img: 'item_1', title: 'Shirt', price: 150 },
@@ -24,7 +21,6 @@ class GoodsList {
             { img: 'item_4', title: 'Shoes', price: 250 },
         ];
     };
-
     render() {
         let listHtml = '';
         this.goods.forEach(good => {
@@ -41,8 +37,61 @@ class GoodsList {
     }
 }
 
+//=======================CART
+
+class CartItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
+    render() {
+        return `<div class="goods-item"><h4>${this.title}</h4><p>$${this.price}</p></div>`;
+    }
+}
+
+class CartList {
+    constructor() {
+        this.cartGoods = [];
+    }
+
+    FetchCart() {
+        this.cartGoods = [
+            { title: 'Shirt', price: 150 },
+            { title: 'Socks', price: 50 },
+            { title: 'Jacket', price: 350 },
+            { title: 'Shoes', price: 250 },
+        ];
+    }
+
+    render () {
+        let cartlist = '';
+        this.cartGoods.forEach(good => {
+            const cartItem = new CartItem(good.title, good.price);
+            cartlist += cartItem.render();
+        });
+        document.querySelector('.cart-main').innerHTML = cartlist;
+    }
+
+}
+const cart = new CartList();
+cart.FetchCart();
+cart.render();
 const list = new GoodsList();
 list.FetchGoods();
 list.render();
 list.totalPrice();
 
+//====================================
+
+const $cartButton = document.querySelector('.cart-button');
+const $cartMain = document.querySelector('.cart-main');
+
+$cartButton.addEventListener('click', () => {
+    if ($cartMain.style.display === 'flex'){
+        $cartMain.style.display = 'none';
+        $cartButton.classList.remove('cart-button_active');
+    } else {
+        $cartMain.style.display = 'flex';
+        $cartButton.classList.add('cart-button_active');
+    }
+});
